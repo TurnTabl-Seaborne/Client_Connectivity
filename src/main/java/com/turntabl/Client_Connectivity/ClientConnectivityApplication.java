58d8f208cc636ade24738813a93dd8c83010f691
Dorothy1111 <dorothyewuah@gmail.com>
2020-11-18 11:30:24 +0000

    Merge branch 'dev' of C:\Users\Dorothy\Documents\Client_Connectivity with conflicts.

--- src/main/java/com/turntabl/Client_Connectivity/ClientConnectivityApplication.java
+++ src/main/java/com/turntabl/Client_Connectivity/ClientConnectivityApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ClientConnectivityApplication {
 	public static void main(String[] args) {
