730ad1833d6fb2e24690588adbbd6a2a714d594f
Nicholas-TurnTabl <nicholas.dickson@turntabl.io>
2020-11-19 21:41:20 +0000

    Added Controller to Send Order recieved from Client Service to Order Validator

--- src/main/java/com/turntabl/Client_Connectivity/clientorder/controller/ClientOrderController.java
+++ src/main/java/com/turntabl/Client_Connectivity/clientorder/controller/ClientOrderController.java
@@ -2,10 +2,17 @@ package com.turntabl.Client_Connectivity.clientorder.controller;
 
 import com.turntabl.Client_Connectivity.clientorder.dao.ClientOrderDao;
 import com.turntabl.Client_Connectivity.clientorder.model.ClientOrder;
+import com.turntabl.Client_Connectivity.clientorder.model.SendOrderResponse;
+import com.turntabl.Client_Connectivity.clientorder.service.WebClientService;
 import com.turntabl.Client_Connectivity.exchangeorder.model.ExchangeOrder;
 import com.turntabl.Client_Connectivity.stockrecord.model.StockRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
@@ -29,8 +36,35 @@ public class ClientOrderController {
         return order.findAllById(id);
     }
     @PostMapping("/api/orders")
-    ClientOrder createOrder(@RequestBody ClientOrder orders){
-        return  order.save(orders);
+    SendOrderResponse createOrder(@RequestBody ClientOrder orders){
+
+        ClientOrder clientOrder;
+        SendOrderResponse sendOrderResponse = new SendOrderResponse();
+
+        WebClient webClient = WebClient.create("http://localhost:8081");
+
+        String order_status = webClient.post()
+                .uri("/api/soap_consumer/sendOrder")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(BodyInserters.fromValue(orders))
+                .retrieve()
+                .bodyToMono(SendOrderResponse.class)
+                .block().getMessage();
+
+        switch(order_status){
+            case("valid"):
+                clientOrder = order.save(orders);
+                sendOrderResponse.setData(clientOrder);
+                sendOrderResponse.setStatusCode(HttpStatus.CREATED.value());
+                sendOrderResponse.setMessage("Order Sent");
+                break;
+            case("invalid"):
+                sendOrderResponse.setStatusCode(HttpStatus.FORBIDDEN.value());
+                sendOrderResponse.setMessage("Invalid Order");
+                break;
+        }
+
+        return sendOrderResponse;
     }
 
     @PutMapping("/api/orders/{id}")
