ec088b754293b9ad06762deda27f2cfaae7592ba
Nicholas-TurnTabl <nicholas.dickson@turntabl.io>
2020-11-20 17:35:04 +0000

    Update ExchangeOrderDao.java

--- src/main/java/com/turntabl/Client_Connectivity/exchangeorder/dao/ExchangeOrderDao.java
+++ src/main/java/com/turntabl/Client_Connectivity/exchangeorder/dao/ExchangeOrderDao.java
@@ -2,6 +2,8 @@ package com.turntabl.Client_Connectivity.exchangeorder.dao;
 
 import com.turntabl.Client_Connectivity.exchangeorder.model.ExchangeOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
