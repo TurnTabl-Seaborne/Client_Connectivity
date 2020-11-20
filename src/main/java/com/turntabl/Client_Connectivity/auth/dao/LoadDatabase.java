e184a247fdb2f486b9ebd157c6bb4522c6b24859
Nicholas-TurnTabl <nicholas.dickson@turntabl.io>
2020-11-20 13:24:08 +0000

    Update LoadDatabase.java

--- src/main/java/com/turntabl/Client_Connectivity/auth/dao/LoadDatabase.java
+++ src/main/java/com/turntabl/Client_Connectivity/auth/dao/LoadDatabase.java
@@ -14,16 +14,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LoadDatabase {
-    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
-
-
-    User user1 = new User();
-    @Bean
-    CommandLineRunner initDatabase(UserRepository repository) {
-
-        return args -> {
-            log.info("Preloading " + repository.save(user1));
-
-        };
-    }
+//    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+//
+//
+//    User user1 = new User();
+//    @Bean
+//    CommandLineRunner initDatabase(UserRepository repository) {
+//
+//        return args -> {
+//            log.info("Preloading " + repository.save(user1));
+//
+//        };
+//    }
 }
